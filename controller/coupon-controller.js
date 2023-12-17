a36ceecef59ce76432ff34f589374a866a57b026
ahmed thousif <ahmedthousif571@gmail.com>
2023-12-17 15:51:19 +0530

    refferal just link done

--- controller/coupon-controller.js
+++ controller/coupon-controller.js
@@ -3,26 +3,7 @@ const Product = require('../models/products-model')
 const User = require('../models/user')
 const Coupon = require('../models/coupon')
 const voucher_codes= require('voucher-code-generator')
-const cron = require('node-cron')
 
-cron.schedule('0 0 * * *', async () => {
-  try {
-    const currentDate = new Date();
-    const expiredCoupons = await Coupon.find({
-      expiry: { $lt: currentDate },
-      status: 1,
-    });
-
-    if (expiredCoupons.length > 0) {
-      await Coupon.updateMany(
-        { _id: { $in: expiredCoupons.map((c) => c._id) } },
-        { $set: { status: 2 } }
-      );
-    }
-  } catch (error) {
-    console.error('Error updating coupon status:', error);
-  }
-});
 
 exports.coupon = async(req,res)=>{
     try{
---
+++ utility/cron.js
@@ -0,0 +1,23 @@
+const cron = require('node-cron')
+
+cron.schedule('0 0 * * *', async () => {
+  try {
+    const currentDate = new Date();
+    const expiredCoupons = await Coupon.find({
+      expiry: { $lt: currentDate },
+      status: 1,
+    });
+
+    if (expiredCoupons.length > 0) {
+      await Coupon.updateMany(
+        { _id: { $in: expiredCoupons.map((c) => c._id) } },
+        { $set: { status: 2 } }
+      );
+    }
+   
+  } catch (error) {
+    console.error('Error updating coupon status:', error);
+  }
+});
+
+module.exports = cron
